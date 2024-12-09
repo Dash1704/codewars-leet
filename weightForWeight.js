d8f1183ac7cf7261214fcb3d98c6c0dad6e316e7
dashboy <dashiellboyles@hotmail.com>
2024-12-09 12:40:47 +0000

    weightForWeight first kata passing

--- weightForWeight.js
+++ weightForWeight.js
@@ -1,19 +1,32 @@
 function orderWeight(string){
-  const numberArray = string.split(" ").map((num) => Number(num))
+  const numberArray = string.split(" ")
+  console.log("numberArray:", numberArray)
 
-  const rankings = {}
+  const rankings = []
 
   numberArray.forEach((num) => {
-    const sum = num.reduce((acc, digit => acc + digit), 0)
-
-    rankings[num] = sum
+    const sum = num.split("").reduce((acc, number) => acc + Number(number), 0)
+    rankings.push([num, sum])
   })
 
+  rankings.sort((a, b) => a[1] - b[1])
+
+  const newArray = []
+  rankings.map((sortedPair) => {
+    newArray.push(sortedPair[0])
+  })
+
+  return newArray.join(" ")
+
   //go through the number array and add into the rankings as a key value pair the number and its new value
+  //{123: 6, 100: 1}
 
-  //return the numbers in their ranking order
+  //Sort the object by ascending order of the value
 
-  return stringSplit
+  //return the keys and join
+  
+
+  
 }
 
 module.exports = orderWeight;
