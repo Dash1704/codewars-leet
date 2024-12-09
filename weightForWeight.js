1433e97981a573f37ed19478905d01c31b15f99e
dashboy <dashiellboyles@hotmail.com>
2024-12-09 13:30:39 +0000

    all tests passed for weight for weight kata

--- weightForWeight.js
+++ weightForWeight.js
@@ -1,32 +1,21 @@
 function orderWeight(string){
   const numberArray = string.split(" ")
-  console.log("numberArray:", numberArray)
 
   const rankings = []
 
   numberArray.forEach((num) => {
     const sum = num.split("").reduce((acc, number) => acc + Number(number), 0)
-    rankings.push([num, sum])
+    rankings.push({number: num, weight: sum})
   })
 
-  rankings.sort((a, b) => a[1] - b[1])
-
-  const newArray = []
-  rankings.map((sortedPair) => {
-    newArray.push(sortedPair[0])
+  rankings.sort((a, b) => {
+    if(a.weight === b.weight){
+      return a.number.toString().localeCompare(b.number.toString());
+    }
+    return a.weight - b.weight
   })
 
-  return newArray.join(" ")
-
-  //go through the number array and add into the rankings as a key value pair the number and its new value
-  //{123: 6, 100: 1}
-
-  //Sort the object by ascending order of the value
-
-  //return the keys and join
-  
-
-  
+  return rankings.map((entry) => entry.number).join(" ")
 }
 
 module.exports = orderWeight;
--- weightForWeight.test.js
+++ weightForWeight.test.js
@@ -3,3 +3,21 @@ const weightForWeight = require('./weightForWeight.js')
 test("it should give weights new order", () => {
   expect(weightForWeight("123 100")).toBe("100 123")
 })
+
+
+test("it should give weights new order", () => {
+  expect(weightForWeight("103 123 4444 99 2000")).toBe("2000 103 123 4444 99")
+})
+
+
+test("it should give weights new order", () => {
+  expect(weightForWeight("103 123 4444 99 2000")).toBe("2000 103 123 4444 99")
+})
+
+test("it should give weights new order", () => {
+  expect(weightForWeight("2000 10003 1234000 44444444 9999 11 11 22 123")).toBe("11 11 2000 10003 22 123 1234000 44444444 9999")
+})
+
+test("it should give weights new order", () => {
+  expect(weightForWeight("90 180")).toBe("180 90")
+})
