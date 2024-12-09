0f64d850a80fa03027146bc8646324cbe5782c57
dashboy <dashiellboyles@hotmail.com>
2024-12-09 15:41:20 +0000

    pig latin kata set up

---
+++ pigIt.test.js
@@ -0,0 +1,5 @@
+const pigIt = require('./pigIt.js');
+
+test("it should move the first letter to the end and add ay", () => {
+  expect(pigIt('Pig latin is cool')).toEqual('igPay atinlay siay oolcay')
+})
