056e28f3e368de4cf8ed58a16d8ce86d508f9d9d
dashboy <dashiellboyles@hotmail.com>
2024-12-09 16:13:20 +0000

    pigLatinComplete

--- pigIt.js
+++ pigIt.js
@@ -1,5 +1,20 @@
-function pigIt() {
-  return "hi"
+function pigIt(str) {
+  arrayOfWords = str.split(" ");
+  const regex = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/g;
+
+  const pigLatinGenerator = arrayOfWords.map((word) => {
+
+    const firstLetter = word.charAt(0)
+    return word.match(regex) ? word : word.slice(1) + firstLetter + "ay"
+  })
+
+  return pigLatinGenerator.join(" ")
+}
+
+//refactored below
+
+function pigIt(str) {
+  return str.replace(/(\w)(\w*)(\s|$)/g, "\$2\$1ay\$3")
 }
 
 module.exports = pigIt
--- pigIt.test.js
+++ pigIt.test.js
@@ -3,3 +3,8 @@ const pigIt = require('./pigIt.js');
 test("it should move the first letter to the end and add ay", () => {
   expect(pigIt('Pig latin is cool')).toEqual('igPay atinlay siay oolcay')
 })
+
+test("it should move the first letter to the end and add ay", () => {
+  expect(pigIt('Hello world !')).toEqual('elloHay orldway !')
+})
+
