6ff9723452b8f40673ba8bab609b9e2be684368f
dashboy <dashiellboyles@hotmail.com>
2024-12-09 14:01:58 +0000

    isbn kata complete

--- isbn.js
+++ isbn.js
@@ -1,5 +1,18 @@
 function validISBN10(isbn) {
-  return false
+  const ISBNArray = isbn.split('')
+  
+  if(ISBNArray.length === 10 && ISBNArray[9] === "X"){
+    ISBNArray[9] = "10"
+  }
+
+  const calculation = ISBNArray.map((number, index) => {
+    const realIndex = index + 1
+    return Number(number * realIndex)
+  })
+
+  const sum = calculation.reduce((acc, sum) => acc + sum, 0)
+
+  return sum % 11 === 0 && ISBNArray.length == 10 ? true : false
 }
 
 module.exports = validISBN10
