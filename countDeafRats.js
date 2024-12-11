74de892c9172592b3fad57af2fd66e4c1f8a0b02
dashboy <dashiellboyles@hotmail.com>
2024-12-11 16:20:02 +0000

    one test passing in countDeafRats

--- countDeafRats.js
+++ countDeafRats.js
@@ -1,5 +1,27 @@
 function countDeafRats(town){
-  return 0
+  const joinedString = town.split('').map((element) => {
+    return element.trim()
+  }).join('')
+  
+  const beforeAndAfterP = joinedString.split("P")
+  
+  let count = 0
+
+  beforeAndAfterP[0].match(/.{1,2}/g).forEach((element) => {
+    if(element === "O~"){
+      count++
+    }
+    return;
+  })
+
+  beforeAndAfterP[1].match(/.{1,2}/g).forEach((element) => {
+    if(element === "~O"){
+      count++
+    }
+    return;
+  })
+
+  return count
 }
 
 module.exports = countDeafRats;
--- countDeafRats.test.js
+++ countDeafRats.test.js
@@ -3,3 +3,11 @@ const countDeafRats = require('./countDeafRats.js')
 test('it should return the number of rats facing the wrong way', () => {
   expect(countDeafRats("~O~O~O~O P")).toBe(0)
 })
+
+test('it should return the number of rats facing the wrong way', () => {
+  expect(countDeafRats("P O~ O~ ~O O~")).toBe(1)
+})
+
+test('it should return the number of rats facing the wrong way', () => {
+  expect(countDeafRats("~O~O~O~OP~O~OO~")).toBe(2)
+})
