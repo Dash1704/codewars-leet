0de6af36b7e3f4a077bd04bdacdd87359dd56feb
dashboy <dashiellboyles@hotmail.com>
2024-12-12 15:44:18 +0000

    completed noStringDuplicates

--- stringArrayDuplicates.js
+++ stringArrayDuplicates.js
@@ -1,5 +1,17 @@
 function noDuplicates(string){
-  return "hui"
+  const newArray = []
+  string.forEach((word) => {
+    const array = []
+    const wordSplit = word.split("")
+    for(let i = 0; i < wordSplit.length; i++){
+      if(wordSplit[i] !== wordSplit[i + 1]){
+        array.push(wordSplit[i])
+      }
+    }
+    newArray.push(array.join(""))
+    
+  })
+  return newArray
 }
 
 module.exports = noDuplicates;
--- stringArrayDuplicates.test.js
+++ stringArrayDuplicates.test.js
@@ -1,5 +1,5 @@
 const noDuplicates = require('./stringArrayDuplicates.js')
 
 test('the string should return no duplicates in the string', () => {
-  expect(noDuplicates(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"])).toBe(['codewars','picaniny','hubububo'])
+  expect(noDuplicates(["ccooddddddewwwaaaaarrrrsssss","piccaninny","hubbubbubboo"])).toEqual(['codewars','picaniny','hubububo'])
 })
