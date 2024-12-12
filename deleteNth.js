67dc9b36a744bf575c3f7ef5c1a607aaa35c6187
dashboy <dashiellboyles@hotmail.com>
2024-12-12 11:11:07 +0000

    alternate method for delete nTh

--- deleteNth.js
+++ deleteNth.js
@@ -1,12 +1,21 @@
 function deleteNth(arr, n){
   const count = {};
   const result = [];
-  for (const number of arr) {
-    count[number] = (count[number] || 0) + 1;
-    if (count[number] <= n) {
-      result.push(number);
+  // for (const number of arr) {
+  //   count[number] = (count[number] || 0) + 1;
+  //   if (count[number] <= n) {
+  //     result.push(number);
+  //   }
+  // }
+
+
+  arr.map((element) => {
+    count[element] = (count[element] || 0) + 1
+    if (count[element] <= n) {
+      result.push(element);
     }
-  }
+  })
+
   return result
 }
 
