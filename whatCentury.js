f664051d4675fc95c63c9034c2999ae71716a140
dashboy <dashiellboyles@hotmail.com>
2024-12-19 10:40:09 +0000

    what century tests passing

--- whatCentury.js
+++ whatCentury.js
@@ -1,5 +1,37 @@
 function whatCentury(year){
- return "20th"
+//need to work out the number of the centuryc
+//1-100 = 1st century
+//101-200 = 2nd century
+//201-300 = 3rd century
+const finalResult = []
+const numberYear = Math.ceil(Number(year) / 100).toString()
+finalResult.push(numberYear)
+
+const yearArray = numberYear.split('')
+// console.log(yearArray)
+console.log(yearArray.slice(-1))
+const arrayLastIndex = yearArray.slice(-1)
+
+if(arrayLastIndex == "1"){
+  finalResult.push("st")
+} else if(arrayLastIndex == "2"){
+  finalResult.push("nd")
+} else if(arrayLastIndex == "3"){
+  finalResult.push("rd")
+} else {
+  finalResult.push('th')
+}
+
+return finalResult.join('')
+
+// const suffix = 
+
+
+
+
+//append onto it the respective suffix of said century
+
+
 }
 
 module.exports = whatCentury;
--- whatCentury.test.js
+++ whatCentury.test.js
@@ -3,3 +3,18 @@ const whatCentury = require('./whatCentury.js')
 test('it should say what century the year is in', () => {
   expect(whatCentury("1999")).toEqual("20th")
 })
+
+test('it should say what century the year is in', () => {
+  expect(whatCentury("2000")).toEqual("20th")
+})
+
+test('it should say what century the year is in', () => {
+  expect(whatCentury("2")).toEqual("1st")
+})
+
+test('it should say what century the year is in', () => {
+  expect(whatCentury("2259")).toEqual("23rd")
+})
+
+
+
