dd3e7e0cb24c8c8e7897de9b12953a946a35c774
dashboy <dashiellboyles@hotmail.com>
2024-12-29 15:05:13 +0000

    bionary array without using strings

--- hammingWeight.js
+++ hammingWeight.js
@@ -1,9 +1,30 @@
+// function hammingWeight(number){
+//   let count = 0
+//   let binary = number.toString(2).split('')
+
+//   for(let i = 0; i < binary.length; i++){
+//     if(binary[i] === "1"){
+//       count++
+//     }
+//   }
+
+//   return count
+// }
+
+//same task but without string operation
+
 function hammingWeight(number){
   let count = 0
-  let binary = number.toString(2).split('')
+  const binaryArray = []
 
-  for(let i = 0; i < binary.length; i++){
-    if(binary[i] === "1"){
+  while (number > 0){
+    const remainder = number % 2
+    binaryArray.push(remainder)
+    number = Math.floor(number / 2)
+  }
+
+  for(let i = 0; i < binaryArray.length; i++){
+    if(binaryArray[i] === 1){
       count++
     }
   }
