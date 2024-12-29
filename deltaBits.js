8852a223e2bac83d69699e752636a4cebd773e7b
dashboy <dashiellboyles@hotmail.com>
2024-12-29 18:03:07 +0000

    passed deltaBits kata

--- deltaBits.js
+++ deltaBits.js
@@ -1,5 +1,46 @@
 function deltaBits(a, b){
-return 2
+  let binaryA = getBinary(a)
+  let binaryB = getBinary(b)
+
+  let biggerArray = undefined
+  let smallerArray = undefined
+
+  if(binaryA.length - binaryB.length > 0){
+    biggerArray = binaryA 
+   smallerArray = binaryB
+  } else {
+    biggerArray = binaryB
+    smallerArray = binaryA
+  }
+
+  while(smallerArray.length < biggerArray.length){
+    smallerArray.push(0)
+  }
+
+  let count = 0
+
+  for(let i = 0; i < biggerArray.length; i++){
+    if(binaryA[i] !== binaryB[i]){
+      count++
+    }
+  }
+
+  return count
+}
+
+function getBinary(num) {
+  binaryArray = []
+  while(num > 0){
+    const remainder = num % 2
+    binaryArray.push(remainder)
+    num = Math.floor(num / 2)
+  }
+
+  return binaryArray
 }
 
 module.exports = deltaBits;
+
+//change both to binary
+//initalise a count
+//iterate over the two arrays add increment count when a change is needed
--- deltaBits.test.js
+++ deltaBits.test.js
@@ -3,3 +3,7 @@ const deltaBits = require("./deltaBits.js")
 test("it should count how changes needed to change a to b in binary", () => {
   expect(deltaBits(31, 14)).toEqual(2)
 })
+
+test("it should count how changes needed to change a to b in binary", () => {
+  expect(deltaBits(7, 17)).toEqual(3)
+})
