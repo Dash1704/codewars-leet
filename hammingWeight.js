a9025d7fdd83db06260bab6ddd3905648c561e10
dashboy <dashiellboyles@hotmail.com>
2024-12-29 14:46:54 +0000

    finished Kata

--- hammingWeight.js
+++ hammingWeight.js
@@ -1,6 +1,14 @@
 function hammingWeight(number){
-  
+  let count = 0
+  let binary = number.toString(2).split('')
 
+  for(let i = 0; i < binary.length; i++){
+    if(binary[i] === "1"){
+      count++
+    }
+  }
+
+  return count
 }
 
 module.exports = hammingWeight;
--- hammingWeight.test.js
+++ hammingWeight.test.js
@@ -8,3 +8,20 @@ test("it should count how mant 1s in a numbers binary form", () => {
 test("it should count how mant 1s in a numbers binary form", () => {
   expect(hammingWeight(21)).toEqual(3)
 })
+
+test("it should count how mant 1s in a numbers binary form", () => {
+  expect(hammingWeight(0)).toEqual(0)
+})
+
+test("it should count how mant 1s in a numbers binary form", () => {
+  expect(hammingWeight(1)).toEqual(1)
+})
+
+test("it should count how mant 1s in a numbers binary form", () => {
+  expect(hammingWeight(2)).toEqual(1)
+})
+
+
+test("it should count how mant 1s in a numbers binary form", () => {
+  expect(hammingWeight(2048)).toEqual(1)
+})
