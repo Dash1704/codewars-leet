59af13971c2e9bc3ce882d73ebef576d5d083c73
dashboy <dashiellboyles@hotmail.com>
2025-01-03 15:27:13 +0000

    refactored rangeBitCounting

--- rangeBitCounting.js
+++ rangeBitCounting.js
@@ -1,18 +1,25 @@
 function rangeBitCount(a, b) {
-  const array = []
+  // const array = []
   let count = 0
+  //1
+  //3
+  //
 
-  for(let i = a; i < b + 1; i++){
-    array.push(i)
+  // for(let i = a; i < b + 1; i++){
+  //   array.push(i)
+  // }
+
+  for(let i = a; i <= b; i++){
+    count += i.toString(2).split('1').length - 1
   }
 
-  for(let i = 0; i < array.length; i++){
-    const binary = array[i].toString(2).split('')
-    for(let i = 0; i < binary.length; i++){
-      if(binary[i] === '1') count++
-    }
-  }
-  
+  // for(let i = 0; i < array.length; i++){
+  //   const binary = array[i].toString(2).split('')
+  //   for(let i = 0; i < binary.length; i++){
+  //     if(binary[i] === '1') count++
+  //   }
+  // }
+
   return count
 }
 
